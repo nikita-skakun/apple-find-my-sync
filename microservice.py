8ba1e2c21a015230ac7a48a662de061d847a30eb
Nikita Skakun <nikita@skakun.io>
2025-12-17 21:25:57 -0800

    Scanner code

--- microservice.py
+++ microservice.py
@@ -151,3 +151,78 @@ def main():
         raise RuntimeError("Push service URL not configured")
 
     return asyncio.run(main_sync())
+
+
+# from __future__ import annotations
+
+# import asyncio
+# import logging
+
+# from findmy import (
+#     FindMyAccessory,
+#     KeyPair,
+#     NearbyOfflineFindingDevice,
+#     OfflineFindingScanner,
+#     SeparatedOfflineFindingDevice,
+# )
+
+# logging.basicConfig(level=logging.INFO)
+
+
+# def _print_nearby(device: NearbyOfflineFindingDevice) -> None:
+#     print(f"NEARBY Device - {device.mac_address}")
+#     print(f"  Status byte:  {device.status:x}")
+#     print("  Extra data:")
+#     for k, v in sorted(device.additional_data.items()):
+#         print(f"    {k:20}: {v}")
+#     print()
+
+
+# def _print_separated(device: SeparatedOfflineFindingDevice) -> None:
+#     print(f"SEPARATED Device - {device.mac_address}")
+#     print(f"  Public key:   {device.adv_key_b64}")
+#     print(f"  Lookup key:   {device.hashed_adv_key_b64}")
+#     print(f"  Status byte:  {device.status:x}")
+#     print(f"  Hint byte:    {device.hint:x}")
+#     print("  Extra data:")
+#     for k, v in sorted(device.additional_data.items()):
+#         print(f"    {k:20}: {v}")
+#     print()
+
+
+# async def scan(check_key: KeyPair | FindMyAccessory | None = None) -> bool:
+#     scanner = await OfflineFindingScanner.create()
+
+#     print("Scanning for FindMy-devices...\n")
+
+#     scan_device = None
+
+#     async for device in scanner.scan_for(10, extend_timeout=True):
+#         if isinstance(device, NearbyOfflineFindingDevice):
+#             _print_nearby(device)
+#         elif isinstance(device, SeparatedOfflineFindingDevice):
+#             _print_separated(device)
+#         else:
+#             print(f"Unknown device: {device}")
+#             print()
+#             continue
+
+#         if check_key and device.is_from(check_key):
+#             scan_device = device
+
+#     print()
+#     if scan_device:
+#         print("Device was found in scan results! :D")
+#     elif check_key:
+#         print("Device was not found in scan results... :c")
+
+#     return scan_device is not None and check_key is not None
+
+
+# def main():
+#     dev = FindMyAccessory.from_json("airtag.json")
+#     asyncio.run(scan(dev))
+
+#     print("Current scan results were used to align the accessory.")
+#     print('Updated alignment will be saved to "airtag.json".')
+#     dev.to_json("airtag.json")
